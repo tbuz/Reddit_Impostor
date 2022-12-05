3376b9f79d045f72db7b7f3e44153ad6c495087e
Nikola Genchev <n.genchev@hotmail.com>
2022-12-05 10:58:29 +0100

    Add script for generating jokes

---
+++ models/gpt2_showerthoughts_runner.py
@@ -0,0 +1,64 @@
+import os
+
+import numpy as np
+import torch
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+
+from utils import Constants
+
+MODEL_EPOCH = input('Enter which EPOCH you would like to use: ')
+
+models_folder = "checkpoints"
+
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2-medium')
+model_path = os.path.join(models_folder, f"gpt2_medium_showerthought_{MODEL_EPOCH}.pt")
+model = GPT2LMHeadModel.from_pretrained(torch.load(model_path))
+
+showerthoughts_output_file_path = f'generated_showerthoughts_{MODEL_EPOCH}.txt'
+
+model.eval()
+if os.path.exists(showerthoughts_output_file_path):
+    os.remove(showerthoughts_output_file_path)
+    
+def choose_from_top(probs, n=5):
+    ind = np.argpartition(probs, -n)[-n:]
+    top_prob = probs[ind]
+    top_prob = top_prob / np.sum(top_prob) # Normalize
+    choice = np.random.choice(n, 1, p = top_prob)
+    token_id = ind[choice][0]
+    return int(token_id)
+
+
+thoughts_num = 0
+with torch.no_grad():
+    
+    for thought_idx in range(100):
+        
+        thought_finished = False
+        cur_ids = torch.tensor(tokenizer.encode("<|showerthought|>")).unsqueeze(0).to(Constants.device)
+        
+        for i in range(100):
+            outputs = model(cur_ids, labels=cur_ids)
+            loss, logits = outputs[:2]
+            softmax_logits = torch.softmax(logits[0,-1], dim=0) #Take the first(from only one in this case) batch and the last predicted embedding
+            if i < 3:
+                n = 20
+            else:
+                n = 3
+            next_token_id = choose_from_top(softmax_logits.to('cpu').numpy(), n=n) #Randomly(from the topN probability distribution) select the next word
+            cur_ids = torch.cat([cur_ids, torch.ones((1,1)).long().to(Constants.device) * next_token_id], dim = 1) # Add the last word to the running sequence
+
+            if next_token_id in tokenizer.encode('<|endoftext|>'):
+                thought_finished = True
+                break
+        
+        if thought_finished:
+                
+            thought_num = thought_num + 1
+            
+            output_list = list(cur_ids.squeeze().to('cpu').numpy())
+            output_text = tokenizer.decode(output_list)
+
+            with open(showerthoughts_output_file_path, 'a') as f:
+                f.write(f"{output_text} \n\n")
+            
--- models/gpt2_showerthoughts_training.py
+++ models/gpt2_showerthoughts_training.py
@@ -81,12 +81,3 @@ for epoch in range(EPOCHS):
     
     # Store the model after each epoch to compare the performance of them
     torch.save(model.state_dict(), os.path.join(Constants.checkpoints_folder, f"gpt2_medium_showerthought_{epoch}.pt"))
-
-
-def choose_from_top(probs, n=5):
-    ind = np.argpartition(probs, -n)[-n:]
-    top_prob = probs[ind]
-    top_prob = top_prob / np.sum(top_prob) # Normalize
-    choice = np.random.choice(n, 1, p = top_prob)
-    token_id = ind[choice][0]
-    return int(token_id)
